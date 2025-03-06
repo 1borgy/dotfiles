0f6d15e6ea146185cf42d6d4d9a1270f1598994c
ellie <149747415+1borgy@users.noreply.github.com>
2025-03-06 17:35:19 -0500

    update

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,8 +2,8 @@ return {
   -- { "rktjmp/lush.nvim", event = "VeryLazy" },
   -- { "rktjmp/shipwright.nvim", cmd = "Shipwright" },
   {
-    -- "1borgy/nyappuccin.nvim",
-    dir = "/Users/ellie/dev/nyappuccin.nvim",
+    "1borgy/nyappuccin.nvim",
+    -- dir = "/Users/ellie/dev/nyappuccin.nvim",
     dependencies = "lush.nvim",
     priority = 10000,
     opts = {},
