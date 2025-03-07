df62b0f6584b3bc1f1ecfaf5d59b02c90647f2c8
ellie <149747415+1borgy@users.noreply.github.com>
2025-03-07 12:01:01 -0500

    update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,8 +4,6 @@ alias activate=". .venv/bin/activate.fish"
 alias act="activate"
 alias deact="deactivate"
 
-alias ssh="TERM=xterm-256color command ssh"
-
 # :3
 alias vim="command nvim"
 alias vi="command nvim"
@@ -13,6 +11,10 @@ alias v="command nvim"
 alias nvi="command nvim"
 alias nv="command nvim"
 alias n="command nvim"
+
+function terminfo
+    infocmp -a $TERM | ssh -vvv $argv 'tic -x -o ~/.terminfo /dev/stdin'
+end
 
 starship init fish | source
 zoxide init fish | source
