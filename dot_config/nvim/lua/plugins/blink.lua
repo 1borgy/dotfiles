e1a3827c2906dd6c5fe50c3931636a2cbcf97c92
ellie <149747415+1borgy@users.noreply.github.com>
2025-03-31 11:05:04 -0400

    update

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,8 +1,85 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "echasnovski/mini.icons",
+    },
+    version = "1.*",
+    event = "VeryLazy",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = "enter" },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "normal",
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = {
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+            components = {
+              kind_icon = {
+                ellipsis = false,
+                text = function(ctx)
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return kind_icon
+                end,
+                -- Optionally, you may also use the highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
+            },
+          },
+          border = "rounded",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+          auto_show = true,
+          auto_show_delay_ms = 0,
+        },
+        ghost_text = { enabled = vim.g.ai_cmp },
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
+  {
+    "saghen/blink.cmp",
     version = "*",
-    -- build = "cargo build --release",
     opts_extend = {
       "sources.completion.enabled_providers",
       "sources.compat",
@@ -36,39 +113,6 @@ return {
         -- adjusts spacing to ensure icons are aligned
         --
         nerd_font_variant = "mono",
-      },
-      completion = {
-        menu = {
-          draw = {
-            treesitter = { "lsp" },
-            components = {
-              kind_icon = {
-                ellipsis = false,
-                text = function(ctx)
-                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                  return kind_icon
-                end,
-                -- Optionally, you may also use the highlights from mini.icons
-                highlight = function(ctx)
-                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-                  return hl
-                end,
-              },
-            },
-          },
-          border = "rounded",
-        },
-        documentation = {
-          window = {
-            border = "rounded",
-          },
-          auto_show = true,
-          auto_show_delay_ms = 0,
-        },
-        ghost_text = {
-          enabled = vim.g.ai_cmp,
-          -- enabled = true,
-        },
       },
 
       -- experimental signature help support
